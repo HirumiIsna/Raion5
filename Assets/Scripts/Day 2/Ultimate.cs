07b6d9faab5c21d5cbf1ab5ae0a3155f5793fe9e
HirumiIsna <hilmiisnaeni@student.ub.ac.id>
2025-11-26 18:07:04 +0700

    Fixed Ultimate Script

--- Assets/Scripts/Day 2/Ultimate.cs
+++ Assets/Scripts/Day 2/Ultimate.cs
@@ -6,51 +6,57 @@ public class Ultimate : MonoBehaviour
 {
     public Transform shootingPoint;
     public Slider ultimateSlider;
-    public float ultimateCharge = 0f;
-
+    public float ultimateCharge = 0f;   
+    private Coroutine ultimateCoroutine;
     void Start()
     {
         ultimateSlider.maxValue = 100f;
-        ultimateSlider.minValue = 0f;
         ultimateSlider.value = ultimateCharge;
+        StartCoroutine(UltimateAttack());
     }
 
     // Update is called once per frame
 
     [SerializeField] private Animator ultAnimator;
-    bool ultReady = false;
 
-    void fillUltimate()
+    private IEnumerator UltimateAttack()
     {
-        ultimateSlider.value = Mathf.Lerp(ultimateSlider.minValue, ultimateSlider.maxValue, 1f * Time.deltaTime);
-    }
-    void resetUltimate()
-    {
-        ultimateSlider.value = ultimateSlider.minValue;
+        while(ultimateCharge < ultimateSlider.maxValue)
+        {
+            ultimateCharge += 1f;
+            ultimateSlider.value = ultimateCharge;
+            yield return new WaitForSeconds(0.25f);
+        }
     }
 
     void Update()
     {
-        fillUltimate();
-
-        if(ultimateCharge >= 100f)
+        Image colorFill = ultimateSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+        if(ultimateSlider.value >= ultimateSlider.maxValue)
         {
-            Debug.Log("Ultimate Ready");
-            ultReady = true;
-        }else{
-            ultReady = false;
+            colorFill.color = Color.green;
         }
-
+        else
+        {
+            colorFill.color = Color.yellow;
+        }
         //ultimate press g
-        if(Input.GetKeyDown(KeyCode.G) && ultReady == true)
+        if(Input.GetKeyDown(KeyCode.G) && ultimateCharge >= ultimateSlider.maxValue)
         {
             ultAnimator.SetBool("isUlt", true);
             ultimateCharge = 0f;
             ultimateSlider.value = ultimateCharge;
-            // resetUltimate();
-            ultReady = false;
+            if(ultimateCoroutine != null)
+            {
+                StopCoroutine(ultimateCoroutine);
+                ultimateCoroutine = null;
+            }
         }else{
-            ultAnimator.SetBool("isUlt", false);;
+            ultAnimator.SetBool("isUlt", false);
+            if(ultimateCoroutine == null)
+            {
+                ultimateCoroutine = StartCoroutine(UltimateAttack());
+            }
         }
     }
 }
